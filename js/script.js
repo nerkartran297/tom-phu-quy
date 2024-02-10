0859b92e3a2f035cc33c62cddd9fdf4cf13a1471
Trần Khánh <nerkar.tran@gmail.com>
2024-02-10 23:07:45 +0700

    DONE DONE

--- js/script.js
+++ js/script.js
@@ -1,34 +1,23 @@
-// const Form1 = document.querySelector("#user_booking_main");
-// Form1.addEventListener("submit", () => {
-//     sendData1();
-// });
+const Form1 = document.querySelector("#user_booking");
 
-// const Form12 = document.querySelector("#user_booking_origin");
-// Form12.addEventListener("submit", () => {
-//     sendData1();
-// });
-
-const Form2 = document.querySelector("#user_contact");
-Form2.addEventListener("submit", () => {
-    sendData2();
+Form1.addEventListener("submit", () => {
+    const urlObject = new URL("https://script.google.com/macros/s/AKfycbyqs1upJ111FDObhzGsvhxz0oxNQSAIfbN_e0zsWNebsYTSg79RKAc5HBhFyE5GR7s49A/exec");
+    const userName = document.getElementById('name').value;
+    const userEmail = document.getElementById('email').value;
+    const userPhone = document.getElementById('phone-number').value;
+    const quantityOfProduct = document.getElementById('quantity').value;
+    const messageOfCustomer = document.getElementById('message').value;
+    urlObject.searchParams.set("userName", userName);
+    urlObject.searchParams.set("userEmail", userEmail);
+    urlObject.searchParams.set("userPhone", userPhone);
+    urlObject.searchParams.set("quantity", quantityOfProduct);
+    urlObject.searchParams.set("messsage", messageOfCustomer);
+    fetch(urlObject.toString());
 });
 
-// const sendData1 = () => {
-//     const urlObject = new URL("https://script.google.com/macros/s/AKfycbyqs1upJ111FDObhzGsvhxz0oxNQSAIfbN_e0zsWNebsYTSg79RKAc5HBhFyE5GR7s49A/exec");
-//     const userName = document.getElementById('name').value;
-//     const userEmail = document.getElementById('email').value;
-//     const userPhone = document.getElementById('phone-number').value;
-//     const quantityOfProduct = document.getElementById('quantity').value;
-//     const messageOfCustomer = document.getElementById('message').value;
-//     urlObject.searchParams.set("userName", userName);
-//     urlObject.searchParams.set("userEmail", userEmail);
-//     urlObject.searchParams.set("userPhone", userPhone);
-//     urlObject.searchParams.set("quantity", quantityOfProduct);
-//     urlObject.searchParams.set("messsage", messageOfCustomer);
-//     fetch(urlObject.toString());
-// };
+const Form2 = document.querySelector("#user_contact");
 
-const sendData2 = () => {
+Form2.addEventListener("submit", () => {
     const urlObject = new URL("https://script.google.com/macros/s/AKfycby5wdinYPwLe-L06u03XQ5p6RZW9q0dfMn_blJxVig3P8Z7QLlRJP3k_LwWCDWNttKu/exec");
     const userName = document.getElementById('name').value;
     const userEmail = document.getElementById('email').value;
@@ -39,4 +28,4 @@ const sendData2 = () => {
     urlObject.searchParams.set("subject", subject);
     urlObject.searchParams.set("messsageCT", messageContact);
     fetch(urlObject.toString());
-};
+});
