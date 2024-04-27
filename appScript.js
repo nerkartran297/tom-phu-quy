e0ab876730bb5b55d5756ae44075bb68073f52f6
Khanh Tran <146191231+nerkartran297@users.noreply.github.com>
2024-04-27 15:15:08 +0700

    Create appScript.js

---
+++ appScript.js
@@ -0,0 +1,89 @@
+var timeZone = "ICT";
+var dateTimeFormat = "dd/MM/yyyy HH:mm";
+
+var enableSendingEmails = true;
+var emailAddress = "";
+
+function doGet(e) {
+      var result = 'Ok'; // default result
+    if (e.parameter == 'undefined') {
+        result = 'No Parameters';
+    } else {
+      
+        var alarm= e.parameter.alarm;
+        if (typeof alarm != 'undefined') {
+
+            sendEmail("alarm text:" + stripQuotes(alarm));
+            return ContentService.createTextOutput(result);
+        }
+
+        var sheet = getSpreadSheet();
+        var lastRow = sheet.getLastRow();
+        var newRow = 1;
+        if (lastRow > 0) {
+            var lastVal = sheet.getRange(lastRow, 1).getValue();
+            if (lastVal == 'dead')
+                newRow = lastRow; //to overwrite "dead" value
+            else
+                newRow = lastRow + 1;
+
+        }
+
+        var rowData = [];
+        var namesOfParams=[];
+        for (var param in parseQuery(e.queryString))
+          namesOfParams.push(param);
+      
+        if (newRow == 1) {
+            rowData[0] = "Date";
+            var i = 1;
+            for (var i=0; i<namesOfParams.length;i++  ) {
+                rowData[i+1] = namesOfParams[i];
+
+            }
+            var newRange = sheet.getRange(newRow, 1, 1, rowData.length);
+            newRange.setValues([rowData]);
+            rowData = [];
+            newRow++;
+        }
+
+        rowData[0] = Utilities.formatDate(new Date(), timeZone, dateTimeFormat);
+    
+        for (var i=0; i<namesOfParams.length;i++  ) {
+
+            var value = stripQuotes(e.parameter[namesOfParams[i]]);
+
+            rowData[i+1] = value;
+
+        }
+        var newRange = sheet.getRange(newRow, 1, 1, rowData.length);
+        newRange.setValues([rowData]);
+    }
+    // Return result of operation
+    return ContentService.createTextOutput(result);
+}
+
+function stripQuotes(value) {
+    return value.replace(/^["']|['"]$/g, "");
+}
+
+function parseQuery(queryString) {
+    var query = {};
+    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        var pair = pairs[i].split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return query;
+}
+
+function sendEmail(message) {
+   if (!enableSendingEmails)
+        return;
+    var subject = 'Something wrong with your esp';
+   MailApp.sendEmail(emailAddress, subject, message);
+}
+
+function getSpreadSheet() {
+    return SpreadsheetApp.getActiveSheet();
+}
